50cd4bc9c38dae1fd791c6dcee1c8c3170207a17
Nero <marcosmarchese04@gmail.com>
2025-06-24 09:52:30 -0300

    changes

--- functtions/loginIndex/pushToDrive.js
+++ functtions/loginIndex/pushToDrive.js
@@ -6,12 +6,7 @@ const fs = require("fs");
 async function pushToDrive() {
     // credenciales
     const credentials = JSON.parse(fs.readFileSync('C:\\Users\\Usuario\\Desktop\\electron\\2\\credentials.json'));
-
-    console.log(credentials)
-
     const { client_secret, client_id, redirect_uris } = credentials.installed;
-    
-    
     const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
     // carga el token de acceso
@@ -32,13 +27,31 @@ async function pushToDrive() {
         body : fs.createReadStream(`\\\\SANDRA\\db\\db.db`)
     }
 
-    const res = await drive.files.create({
-        resource : fileMetadata,
-        media : media,
-        fields : 'id'
-    })
+    // Corrobora si existe el archivo
+    const listRes = await drive.files.list({
+        q: `name='db.db' and trashed=false`,
+        fields: 'files(id , name)'
+    });
 
-    console.log("archivo subido con id: ", res.data.id);
+    // si existe lo actualiza
+    if (listRes.data.files.length>0){
+        const fileId = listRes.data.files[0].id;
+        const updateRes = await drive.files.update({
+            fileId   : fileId,
+            media   : media
+        })
+    }
+    else{
+        // si no existe lo crea
+        const res = await drive.files.create({
+            resource : fileMetadata,
+            media : media,
+            fields : 'id'
+        })
+    }
+
+
+    console.log("el archivo db.db se subió al drive  de marcosmarchese04@gmail.com");
 }
 
 
